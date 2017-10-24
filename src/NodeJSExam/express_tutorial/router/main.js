1cbc952b3f3309fedf02d1b1a3f1faba993b4476
sangki <sang5052003@rosesystems.kr>
2017-10-24 18:02:06 +0900

    Node Js Exam express_tutorial

--- src/NodeJSExam/express_tutorial/router/main.js
+++ src/NodeJSExam/express_tutorial/router/main.js
@@ -1,8 +1,181 @@
-module.exports = function (app) {
+module.exports = function (app, fs) {
+    // app.get('/', function (req, res) {
+    //     res.render('index.html');
+    // });
+    // app.get('/about', function (req, res) {
+    //     res.render('about.html')
+    // })
+
     app.get('/', function (req, res) {
-        res.render('index.html');
+
+        var sess = req.session;
+
+        res.render('index', {
+            title: 'MY HOMEPAGE',
+            length: 5,
+            name: sess.name,
+            username: sess.username
+        });
     });
-    app.get('/about', function (req, res) {
-        res.render('about.html')
+
+    // ./ 현재 폴더
+    // /../ 상위 폴더 접근
+    app.get('/list', function (req, res) {
+        fs.readFile(__dirname + '/../data/' + 'user.json', 'utf8', function (err, data) {
+            console.log(data);
+            res.end(data);
+        });
+    });
+
+    app.get('/getUser/:username', function (req, res) {
+        fs.readFile(__dirname + "/../data/user.json", 'utf8', function (err, data) {
+
+            var users = JSON.parse(data); //json 형식의 텍스트 형태 -> json 파일로..
+
+            console.log(req.params.username);        // :username url 로 넘어온 값
+            console.log(users[req.params.username]); // 값의 json객체
+
+            res.json(users[req.params.username]);
+        });
+    });
+
+    app.post('/addUser/:username', function (req, res) {
+
+        var result = {};
+        var username = req.params.username;
+
+        //check req 유효한지
+        if (!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        //이미 존재하는 데이터인지 체크
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function (err, data) {
+            var users = JSON.parse(data);
+            if (users[username]){
+
+                result["success"] = 0;
+                result["error"] = "duplicate";
+                res.json(result);
+                return;
+            }
+
+            //데이터를 쓰고            
+            users[username] = req.body;
+
+            //데이터 저장
+            fs.writeFile(__dirname + "/../data/user.json",
+                JSON.stringify(users, null, '\t'), //json의 pretty-print를 위함
+                "utf8", function (err, data) {
+                    result = {"success": 1};
+                    res.json(result);
+                })
+        })
+    });
+
+    // PUT API 는 idempotent 해야 합니다,
+    // 쉽게말하자면 즉 요청을 몇번 수행하더라도, 같은 결과를 보장해야합니다.
+    app.put('/updateUser/:username', function (req,res) {
+
+        var result = {};
+        var username = req.params.username;
+
+        if (!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function (err, data) {
+            var users = JSON.parse(data);
+            users[username] = req.body;
+
+            fs.writeFile(__dirname + "/../data/user.json",
+                JSON.stringify(users, null, '\t'), "utf8", function (err, data) {
+                    result = {"success": 1};
+                    res.json(result);
+                })
+        })
+    });
+
+    app.delete('/deleteUser/:username', function (req, res) {
+        var result = {};
+
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function (err, data) {
+            var users = JSON.parse(data);
+
+            if (!users[req.params.username]){
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            delete users[req.params.username];
+            fs.writeFile(__dirname + "/../data/user.json",
+                JSON.stringify(users, null, '\t'), "utf8", function (err, data) {
+                    result["success"] = 0;
+                    res.json(result);
+                    return;
+                })
+        })
+    });
+
+    //session
+    app.get('/login/:username/:password', function (req, res) {
+        var sess = req.session;
+
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function (err,data) {
+
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+
+            //username에 해당하는 json객체 가 없는 경우
+            if (!users[username]){
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            //password 맞으면
+            if (users[username]["password"] == password){
+                result["success"] = 1;
+                //session 에 username 과 name 을 저장
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+            }else{
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        })
+    });
+    
+    app.get("/logout", function (req, res) {
+        sess = req.session;
+        //session 에 username 이 저장되어 있다면
+        if (sess.username){
+            req.session.destroy(function (err) {
+                if (err){
+                    console.log(err);
+                }else{
+                    res.redirect('/');
+                }
+            })
+        }else{
+            res.redirect('/');
+        }
     })
 }
+
+// JSON 데이터를 render 메소드의 두번째 인자로 전달
+// 함으로서 페이지에서 데이터를 사용가능
+// index.ejs 에서 title, length 사용
--- src/NodeJSExam/express_tutorial/server.js
+++ src/NodeJSExam/express_tutorial/server.js
@@ -2,24 +2,41 @@
 var express = require('express');
 var app = express();
 
-//브라우저에서 Request가 왔을 대 서버에서 어떤 작업을 할지 Router를 통하여 설정
-var router = require('./router/main')(app);
+//추가 모듈 부르기
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require('fs'); //node js 내장 모듈, 파일 열기
+// Express 의 이전 버전에서는 cookie-parser 모듈도 불러와야했지만,
+// 이젠 express-session 모듈이 직접 쿠키에 접근하므로 cookie-parser 를 더이상 사용 할 필요가 없다
 
 // __dirname
 // The directory name of the current module
 // This the same as the path.dirname() of the __filename
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views'); //서버가 읽을 수 있도록 HTML 의 위치를 정의
+//서버가 HTML 랜더링을 할 때, EJS 엔진을 사용하도록 설정
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static('public'));
-
 var server = app.listen(3000, function () {
-
     console.log('Express server ahs started on 3000 port');
 });
 
-//split
+// 정적파일(Static files) : HTML 에서 사용되는 .js 파일, css 파일, image 파일 등
+app.use(express.static('public')); //css dir 위치가 public에 있다
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$', //쿠키를 임의로 변조하는것을 방지하기 위한 sign 값. 원하는 값을 넣으면 된다
+    resave: false, //세션을 언제나 저장할 지 (변경되지 않아도) 정하는 값
+    saveUninitialized: true // 세션이 저장되기 전에 uninitialized 상태로 미리 만들어서 저장
+}));
+
+//브라우저에서 Request 가 왔을 대 서버에서 어떤 작업을 할지 Router 를 통하여 설정
+//router 모듈 불러서 app 에 전달
+var router = require('./router/main')(app, fs); //router 에서 fs를 사용할 수 있도록 추가
+
+//split - router 로 split 시킴
 //
 // app.get('/', function (req, res) {
 //     res.send('hellow');
